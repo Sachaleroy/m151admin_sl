fa44e343bd1beec305a963069779270eb23afcf2
Sachaleroy <sacha.lr@eduge.ch>
2015-08-26 14:59:40 +0200

    Formulaire fini

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>m151admin_sl</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/style.css" type="text/css">
+    </head>
+    <body>
+        <div id="content">
+            <label class="labelSize">Nom :</label>
+            <input type="text" class="InputFormulaire"><br/>
+            
+            <label class="labelSize">Prénom :</label>
+            <input type="text" class="InputFormulaire"><br/>
+            
+            <label class="labelSize">Date de naissance :</label>
+            <input type="date" class="InputFormulaire"><br/>
+            
+            <label for="description" class="labelSize">Mini description :</label>
+            <textarea id="description" class="InputFormulaire"></textarea><br/>
+            
+            <label class="labelSize">Email :</label>
+            <input type="email" class="InputFormulaire"><br/>
+            
+            <label class="labelSize">Pseudo :</label>
+            <input type="text" class="InputFormulaire"><br/>
+            
+            <label class="labelSize">Mot de passe :</label>
+            <input type="password" class="InputFormulaire"><br/>
+            
+            <input type="button" value="Envoyer">
+            <input type="button" value="Réinitialiser">
+        </div>
+    </body>
+</html>
