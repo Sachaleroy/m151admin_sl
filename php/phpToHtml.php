caa533590f56a8820ee1ab18f52f7226ce14059d
Sachaleroy <sacha.lr@eduge.ch>
2015-09-23 15:39:58 +0200

    Ajout de la fonction pour l'affichage des détails d'un utilisateur

--- php/phpToHtml.php
+++ php/phpToHtml.php
@@ -1,10 +1,21 @@
 <?php
 function createTableUser($table)
 {
+    //Pour chaque entrée dans la base...
     foreach($table as $row)
     {
-        echo"<tr> <td>".$row[0]."</td> <td>".$row[1]."</td> <td>".$row[2]."</td> <td>".$row[3]."</td> <td>".$row[4]."</td> <td>".$row[5]."</td> <td>".$row[6]."</td> <td><a href='detailsUser".$row[0].".php'>|_۞_|</a></td> <td><a href='modifUser".$row[0].".php'>|_۞_|</a></td> </tr>";
+        //...on crée une ligne de tableau
+        echo"<tr> <td>".$row[0]."</td> <td>".$row[1]."</td> <td>".$row[2]."</td> <td>".$row[3]."</td> <td>".$row[4]."</td> <td>".$row[5]."</td> <td><a href='detailsUser.php?page=".$row[0]."'>D</a></td> <td><a href='../index.php?modif=".$row[0]."'>M</a></td> </tr>";
     }
     //echo"<tr> <td>".$row[0]."</td> <td>".$row[1]."</td> <td>".$row[2]."</td> <td>".$row[3]."</td> <td>".$row[4]."</td> <td>".$row[5]."</td> <td>".$row[6]."</td> </tr>";
+}
+
+//Fonction qui affiche les infos d'un utilisateur précis
+function createTableDetailsUser($table)
+{
+    foreach($table as $row)
+    {
+        echo"<tr> <td>".$row[0]."</td> <td>".$row[1]."</td> <td>".$row[2]."</td> <td>".$row[3]."</td> <td>".$row[4]."</td> <td>".$row[5]."</td> <td>".$row[6]."</td> </tr>";
+    }
 }
 
