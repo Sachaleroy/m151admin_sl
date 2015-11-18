92b792176397efd62fac1dcfbf841c2218f68a34
Sachaleroy <sacha.lr@eduge.ch>
2015-11-18 16:07:51 +0100

    Création de la page de choix des sports + formulaire avec liste de choix des sports

---
+++ php/sports.php
@@ -0,0 +1,63 @@
+<?php
+require_once 'dbFunctions.php';
+require_once "phpToHtml.php";
+session_start();
+
+if(!isset($_SESSION['login'])){
+    header('Location: ./utilisateurs.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>m151admin_sl</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css" type="text/css">
+    </head>
+    <body>
+        <?php
+        if(!isset($_SESSION['login']))
+        {
+            echo "<a href='./login.php' style='text-decoration: none;'>Connexion</a>";
+        }
+        if(isset($_SESSION['login']))
+        {
+            echo "Utilisateur connecté : ".$_SESSION['login']." |   <a href='./deconnexion.php' style='text-decoration: none;'>Déconnexion</a>";
+        }
+        ?>
+        <div id="content">
+            <h1>Choix des sports</h1>
+            <form action="./php/dbFunctions.php" method="post">
+                <label for="choix1" class="labelSelectSize">Choix 1 :</label>
+                <select name="choix1">
+                    <?php
+                    CreateListSports(Listsports());
+                    ?>
+                </select><br/>
+                <label for="choix2" class="labelSelectSize">Choix 2 :</label>
+                <select name="choix2">
+                    <?php
+                    CreateListSports(Listsports());
+                    ?>
+                </select><br/>
+                <label for="choix3" class="labelSelectSize">Choix 3 :</label>
+                <select name="choix3">
+                    <?php
+                    CreateListSports(Listsports());
+                    ?>
+                </select><br/>
+                <label for="choix4" class="labelSelectSize">Choix 4 :</label>
+                <select name="choix4">
+                    <?php
+                    CreateListSports(Listsports());
+                    ?>
+                </select><br/>                
+                <input type="submit" value="Envoyer choix" name="envoyerChoix" >
+                <input type="reset" value="Réinitialiser">
+                <a href="utilisateurs.php">liste d'utilisateurs</a>
+            </form>
+        </div>
+    </body>
+</html>
+
